0a88006fb233b0090151f7c64ca365dcae6c98f3
Денис OverkillZ <D0027@yandex.ru>
2025-07-29 23:34:01 +0300

    Добавил H1 на главную страницу

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $titlePage = 'Главная';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/header.php';
 ?>
 
+<h1>Привет всем!</h1>
 
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php';
